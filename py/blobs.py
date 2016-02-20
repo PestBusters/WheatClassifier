9fe894c824be8f97ed4dde4077107259ee092616
Andrii Lashchenko <andrew.lashchenko@gmail.com>
2016-02-20 14:09:35 +0200

    [example] update blobs

--- py/blobs.py
+++ py/blobs.py
@@ -1,14 +1,33 @@
-from math import sqrt
-
+import numpy as np
 import matplotlib.pyplot as plt
+from math import sqrt
 from scipy import misc
+from skimage import io
 from skimage import color
 from skimage import feature
+from skimage import restoration
+from skimage import filter
 
-image = misc.imread('../images/wheat3.jpg')
+# image = misc.imread('../images/wheat3.jpg')
+# image = files = io.ImageCollection('../images/examples/pure' + '*.jpg')[0]
+# image = io.ImageCollection('../images/examples/pure/' + '*.JPG')[1]
+# image = io.ImageCollection('../images/examples/pure/' + 'SAM_0681.JPG')[0]
+# image = misc.imread('../images/examples/pure/' + 'SAM_0681.JPG')
+image = misc.imread('../images/examples/15p/' + 'SAM_0712.JPG')
+plt.imshow(image)
+plt.show()
 
-image_gray = color.rgb2gray(image)
-# image_gray = color.rgb2hsv(image)[:, :, 1]  # HUE
+# image_gray = color.rgb2gray(image)
+image_gray = color.rgb2hsv(image)[:, :, 1]  # HUE
+image_gray = image_gray[100:200, 100:200]
+
+filter_blurred_f = filter.gaussian_filter(image_gray, 1)
+alpha = 0.1
+sharpened = image_gray + alpha * (image_gray - filter_blurred_f)
+image_gray = sharpened
+# psf = np.ones((5, 5)) / 25
+# image_gray, _ = restoration.unsupervised_wiener(image_gray, psf)
+
 plt.imshow(image_gray, cmap='gray')
 plt.show()
 
